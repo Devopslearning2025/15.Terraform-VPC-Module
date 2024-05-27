b7d269c5d63f503793f2bd15441636c1c84dbcd9
Ramesh <dev0pslearning2025@gmail.com>
2024-05-27 09:11:15 +0530

    vpc-module

--- output.tf
+++ output.tf
@@ -33,3 +33,7 @@ output "nat_gateway_id" {
 output "public_route_table_id" {
     value = aws_route_table.public.id
 }
+
+output "database_subnet_group_name" {
+    value = aws_db_subnet_group.default.name
+}
